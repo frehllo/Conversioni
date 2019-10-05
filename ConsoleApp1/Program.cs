110cc6b0e3aa26ac28082905eec1f92b4c6d2248
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-05 11:23:53 +0200

    You can convert to binary from decimal.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the number to convert to binary: ");
+            int decimale = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(decimale, 2);
+            Console.WriteLine($"The converted number is {binary}");
+            Console.ReadLine();
+        }
+    }
+}
